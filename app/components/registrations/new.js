3f5e2bb256b3acb7241bf34c5ff9790a815c7a42
Keith Alpichi <keithalpichi@gmail.com>
2017-01-20 16:37:02 -0800

    add sign-in and sign-up component views

--- app/components/registrations/new.js
+++ app/components/registrations/new.js
@@ -1,7 +1,74 @@
 import React, { Component } from 'react'
+import { setDocumentTitle, renderErrorsFor } from '../../utils'
+import { connect }              from 'react-redux'
+import { Link }                 from 'react-router'
 
-export default class RegistrationsNew extends Component {
+class RegistrationsNew extends Component {
+  componentDidMount() {
+    setDocumentTitle('Sign up')
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault()
+    const data = {
+      user_name: this.refs.userName.value,
+      first_name: this.refs.firstName.value,
+      last_name: this.refs.lastName.value,
+      email: this.refs.email.value,
+      password: this.refs.password.value,
+      password_confirmation: this.refs.passwordConfirmation.value
+    }
+    //this.props.signUp(data)
+    console.warn('Submitting form: Set this.props.signUp action')
+  }
+
   render() {
-    return (<h2>Register here</h2>)
+    const { errors } = this.props
+
+    return (
+      <div className="view-container">
+          <h1>Start here</h1>
+
+          <form onSubmit={::this._handleSubmit}>
+            <div className="field">
+              <input ref="firstName" type="text" placeholder="First name" required={true}/>
+              {renderErrorsFor(errors, 'first_name')}
+            </div>
+            <div className="field">
+              <input ref="lastName" type="text" placeholder="Last name" required={true}/>
+              {renderErrorsFor(errors, 'last_name')}
+            </div>
+            <div className="field">
+              <input ref="userName" type="text" placeholder="Username" required={true}/>
+              {renderErrorsFor(errors, 'user_name')}
+            </div>
+            <div className="field">
+              <input ref="email" type="email" placeholder="Email" required={true}/>
+              {renderErrorsFor(errors, 'email')}
+            </div>
+            <div className="field">
+              <input ref="password" type="password" placeholder="Password" required={true}/>
+              {renderErrorsFor(errors, 'password')}
+            </div>
+            <div className="field">
+              <input ref="passwordConfirmation" type="password" placeholder="Confirm password" required={true}/>
+              {renderErrorsFor(errors, 'password_confirmation')}
+            </div>
+
+            <button type="submit">Sign up</button>
+          </form>
+          <Link to="/sign-in">Sign in</Link>
+      </div>
+    )
   }
 }
+
+const mapStateToProps = (state) => ({
+  errors: state.errors
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  signUp: (data) => dispatch(/*dispatch signUp action with (data)*/)
+})
+
+export default connect(mapStateToProps)(RegistrationsNew)
--- app/components/sessions/new.js
+++ app/components/sessions/new.js
@@ -1,7 +1,57 @@
 import React, { Component } from 'react'
+import { connect }          from 'react-redux';
+import { Link }             from 'react-router';
+import { setDocumentTitle } from '../../utils';
 
-export default class SessionsNew extends Component {
+class SessionsNew extends Component {
+  componentDidMount() {
+    setDocumentTitle('Sign in')
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault()
+    const { email, password } = this.refs;
+    //this.props.signIn(email.value, password.value)
+    console.warn('implement handle submit')
+  }
+
+
   render() {
-    return (<h2>Sign in here</h2>)
+    return (
+      <div className="view-container">
+        <h1>Sign in here</h1>
+        <form id="sign_in_form" onSubmit={::this._handleSubmit}>
+            <div className="field">
+              <input
+                ref="email"
+                type="Email"
+                id="user_email"
+                placeholder="Email"
+                required="true"
+              />
+            </div>
+            <div className="field">
+              <input
+                ref="password"
+                type="password"
+                id="user_password"
+                placeholder="Password"
+                required="true"
+              />
+            </div>
+            <button type="submit">Sign in</button>
+          </form>
+          <Link to="/sign-up">Create new account?</Link>
+      </div>
+    )
   }
 }
+const mapStateToProps = (state) => ({
+  session: state.errors
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  signIn: (email, password) => dispatch(/*dispatch action*/)
+})
+
+export default connect(mapStateToProps)(SessionsNew)
